9c12507177d37271f160dc7fda7610066bc8583e
Zelioz273 <41243839+Zelioz273@users.noreply.github.com>
2018-07-19 14:37:06 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
 
-const config = require("./config.json");
+const { prefix, token } = require("./config.json");
 client.commands = new Discord.Collection();
 
 // This loop reads the /events/ folder and attaches each event file to the appropriate event.
@@ -47,4 +47,4 @@ client.on("message", message => {
 	}
 });
 
-client.login(config.token);
+client.login(token);
